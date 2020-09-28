963e048e914691f34fc708fbed52d3e3fd2c557c
Denis Timofeev <y719xy@gmail.com>
2020-09-28 20:13:12 +0300

    add JUnit 4 \ JUnit 5 dependencies add JUnit 5 tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -2,6 +2,7 @@ package ru.netology.service;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
@@ -10,7 +11,7 @@ public class CashbackHackServiceTest {
     public void shouldReturnNull() {
         int actual = service.remain(1000);
         int expected = 0;
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
         //TODO: You need to fix that         28.09.2020
     }
 
@@ -18,6 +19,19 @@ public class CashbackHackServiceTest {
     public void shouldReturn200() {
         int actual = service.remain(800);
         int expected = 200;
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldReturn200v2() {
+        int actual = service.remain(800);
+        Assertions.assertEquals(200, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldReturnNullV2() {
+        int actual = service.remain(1000);
+        Assertions.assertEquals(0, actual);
+        //TODO: You need to fix that         28.09.2020
     }
 }
